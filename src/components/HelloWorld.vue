6ad83e543825aece4c945a0da95023f5fdb796ce
Saba Tahir <sata@ciklum.com>
2021-12-13 15:36:59 +0500

    fix

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{msg}}</h1>
+    <input v-model="value"
+    type="checkbox"
+    value="a"
+    />
+       <input v-model="value"
+    type="checkbox"
+    value="b"
+    />
+    {{value}}
+    <div class="red">{{error}}</div> -->
+<!-- <h3>V-if,else,for</h3>
+ <button v-on:click="increment">Increment</button>
+<p>{{count}}</p> -->
+
+
+<!-- <h3>Computed properties</h3>
+<div v-for="num in evenList" v-bind:key="num.id">
+  <p>{{num}} </p>
+
+</div> -->
+<Number 
+v-for="num in numbers" v-bind:key="num.id"
+v-bind:num="num"
+v-on:chosen="addNumber"
+/>
+<hr/>
+<Number 
+v-for="num in historyNumber" v-bind:key="num.id"
+v-bind:num="num"
+
+/>
+  </div>
+</template>
+
+<script>
+import Number from './Number.vue';
+export default {
+  name: 'HelloWorld',
+  components:{
+    Number
+    },
+  props:{
+    msg:String
+  },
+  data(){
+    return {
+      count: 0,
+      value: ["a"],
+      numbers:[1,2,3,4,5,6,7,8,9,10],
+      historyNumber:[]
+    
+    }
+  },
+  computed :{
+    evenList(){
+      return this.numbers.filter(num => this.isEven(num));
+      },
+      error(){
+      if(this.value.length < 5){
+       return 'Must be greater than five';
+      }
+      else{
+        return '';
+      }}
+      },
+
+  methods:{
+    increment(){
+      this.count +=1
+  },
+addNumber(num){
+  this.historyNumber.push(num)
+console.log("num",num)
+  }
+  }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.blue{
+  color: blue;
+  }
+.red{
+  color: red
+  }
+</style>
---
+++ src/components/Number.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+<button 
+   v-bind:class="getClass(num)"
+   v-on:click="click(num)">
+   {{num}}
+  <!-- <span v-if="Even(num)">Even</span>
+    <span v-else>Odd</span> -->
+ </button>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Number',
+    props:{
+    num:Number
+  },
+ 
+ methods:{
+     click(){
+         this.$emit('chosen',this.num)
+       
+     },
+    getClass(num){
+      return this.isEven(num) ? 'blue': 'red';
+    },
+    isEven(num){
+   return num%2 === 0;
+  
+  }
+}
+}
+</script>
+<style scoped>
+.blue{
+  color: blue;
+  }
+.red{
+  color: red
+  }
+</style>
+
