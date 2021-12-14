6d9740daf8a718219757bde9b10db3e56d8ce40e
Saba Tahir <sata@ciklum.com>
2021-12-14 12:35:07 +0500

    small input project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <form @submit.prevent="submit">
+      <my-input 
+        name="Username" 
+        :rules="{ required: true, min: 5 }"
+        :value="username.value"
+        type="text"
+        @update="update"
+      />
+
+      <my-input 
+        name="Password" 
+        :rules="{ required: true, min: 10 }"
+        :value="password.value"
+        type="password"
+        @update="update"
+      />
+
+      <my-button 
+        color="white"
+        background="darkslateblue"
+        :disabled="!valid"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyButton from './components/form-validation/Button.vue'
+import MyInput from './components/form-validation/My-Input.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MyButton,
+    MyInput
+  },
+  data() {
+    return {
+      username: {
+        value: '',
+        valid: false
+      },
+      password: {
+        value: '',
+        valid: false
+      }
+    }
+  },
+  computed: {
+    valid() {
+      return this.username.valid && this.password.valid
+    }
+  },
+  methods: {
+    submit() {
+      console.log('Submit')
+    },
+    update(payload) {
+      this[payload.name] = {
+        value: payload.value,
+        valid: payload.valid
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 200px;
+}
+body {
+  font-family: Arial;
+}
+form {
+  max-width: 400px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
