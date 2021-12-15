933a9f5ed261ca2c0201ba7c068ab8281eb38c1f
Saba Tahir <sata@ciklum.com>
2021-12-15 12:44:31 +0500

    learn compostion api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+   <button @click="a++">{{a}}</button>
+
+  <button @click="b++">{{b}}</button>
+  <p>{{total}}</p>
+  <div v-for="number in history" :key="number.id">
+    {{number}}
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { useNumbers } from './components/UseNumber'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ setup(){
+   //ref is used for primitive values like num, string etc
+ 
+   //reactive is used for complex values array, objects etc
+  //  const numbers = reactive({
+  //    a:0,
+  //    b:0
+  //  })
+
+ 
+  //  const increase =(n)=>{
+  //    numbers[n]++
+  //  }
+   //if any change in count , a and b it update the  dom according to that
+   
+  const {a, b,double, history} = useNumbers()
+
+
+   return {
+      a, b, history,double
+    }
+ }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+p{
+  font-size: 40px;
+}
+button{
+  height: 200px;
+  width:200px;
+  font-size: 40px;
 }
 </style>
